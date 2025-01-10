e7230fa3a1a6d861bfaf78bde562af50fc091d7b
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2025-01-10 21:11:13 +1100

    minor fix

--- launch/system.launch.py
+++ launch/system.launch.py
@@ -86,7 +86,7 @@ def generate_launch_description():
         description='Whether to respawn if a node crashes. Applied when composition is disabled.')
 
     declare_log_level_cmd = DeclareLaunchArgument(
-        'log_level', default_value='debug',
+        'log_level', default_value='info',
         description='log level')
     
     declare_use_namespace_cmd = DeclareLaunchArgument(
